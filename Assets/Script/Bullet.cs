577565088d0e2115f4a26a12f5990d674f65e395
Saka_Jimin <Saka_Jimin@DESKTOP-N6DRPP6.com>
2018-06-23 10:06:20 +0900

    1111

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    //BulletController
+    //이지민 최초수정(2018.5.26)
+    private const float moveSpeed = 0.45f;
+    public GameObject gameObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+        float moveX = moveSpeed * Time.DeltaTime();
+        transform.Translate(vector3.right)
+    }
+}
---
+++ Assets/Script/anenmy.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anenmy : MonoBehaviour {
+
+    private double health;
+    private double movSpeed;
+    private double atkSpeed;
+    private int enermyX;
+    private int enermyY;
+
+
+    //체력관련 함수
+    public double Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+        }
+    }
+
+
+
+    //이동속도관련 함수
+    public double MovSpeed
+    {
+        get
+        {
+            return movSpeed;
+        }
+        set
+        {
+            movSpeed = value;
+        }
+    }
+
+
+    //공격속도관련 함수
+    public double AtkSpeed
+    {
+        get
+        {
+            return atkSpeed;
+        }
+        set
+        {
+            atkSpeed = value;
+        }
+    }
+
+
+    //적 x좌표
+    public int EnermyX
+    {
+        get
+        {
+            return enermyX;
+        }
+
+        set
+        {
+            enermyX = value;
+        }
+    }
+
+
+    //적 Y좌표
+    public int EnermyY
+    {
+        get
+        {
+            return enermyY;
+        }
+
+        set
+        {
+            enermyY = value;
+        }
+    }
+
+
+    //공격
+    public int Attack() {
+
+
+        return 1;
+
+    }
+
+
+    //적쫒기
+    public int Chaese() {
+
+
+        return 1;
+
+    }
+
+}
